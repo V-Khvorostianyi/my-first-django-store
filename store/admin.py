4ebc279ee630d3d3c35fcd324ca922e562916d3f
ViacheslavKhvorostyanyi <slavafh@gmail.com>
2017-08-01 08:31:44 +0300

    changes in the idmin views, bootstrap page, reserved old page

--- store/admin.py
+++ store/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Subscriber)
+class SubscribleAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in Subscriber._meta.fields]
+	list_filter = ('name',)
+	search_fields = ['name']
+	class Meta:
+		model = Subscriber
+	
+admin.site.register(Subscriber, SubscribleAdmin)
