57dafb4a598392fcf108298aa922f1433c31aea4
ViacheslavKhvorostanyi <slavafh@gmail.com>
2017-08-21 18:03:49 +0300

    add some edit

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     url(r'^', include('store.urls')),
     # url(r'^', include('products.urls')),
     # url(r'^', include('orders.urls')),
-] \
-              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+]              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^landing/$', views.store_page, name='store_page'),
-]
+] \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
