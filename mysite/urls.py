77b96f279cd698b6921cfe89c1c42a83214fe293
ViacheslavKhvorostanyi <slavafh@gmail.com>
2017-08-25 00:24:34 +0300

    add views and urks to product app

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('store.urls')),
-    # url(r'^', include('products.urls')),
+    url(r'^', include('products.urls')),
     # url(r'^', include('orders.urls')),
 ]              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from products.models import *
+
+
+def product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'products/product.html', locals())
+
