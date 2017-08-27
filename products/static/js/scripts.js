98e4fb6801aa1d9e25b19e609f8167d2dbf1f9cd
ViacheslavKhvorostynyi <slavafh@gmail.com>
2017-08-27 09:06:21 +0300

    edit script.js

--- products/static/js/scripts.js
+++ products/static/js/scripts.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
     var form;
     form = $('#form_buying_product');
     console.log(form);
-})
+    form.on('submit',function (e) {
+
+        console.log('123')
+    })
+});
