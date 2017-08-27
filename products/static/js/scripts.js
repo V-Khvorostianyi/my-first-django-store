f0f224c79bdf3d52fea70829a9fc6e058a41d7d4
ViacheslavKhvorostynyi <slavafh@gmail.com>
2017-08-27 18:27:55 +0300

    jqery script edit

--- products/static/js/scripts.js
+++ products/static/js/scripts.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
     form = $('#form_buying_product');
     console.log(form);
     form.on('submit',function (e) {
-
-        console.log('123')
-    })
+        e.preventDefault();
+        console.log('123');
+        var qty = $('#number').val();
+        console.log(qty);
+    });
 });
