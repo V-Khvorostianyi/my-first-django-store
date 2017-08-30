625db0643456182d52aacbabe9bc7423debc78ab
ViacheslavKhvorostynyi <slavafh@gmail.com>
2017-08-30 06:41:01 +0300

    del console log

--- products/static/js/scripts.js
+++ products/static/js/scripts.js
@@ -3,18 +3,23 @@ $(document).ready(function() {
     var form;
     form = $('#form_buying_product');
     console.log(form);
-    form.on('submit',function (e) {
-        e.preventDefault();
-        var qty = $('#number').val();
-        console.log(qty);
-        var submit_btn = $('#submit-btn');
-        var name = submit_btn.data('name');
-        var product_id = submit_btn.data('product_id');
-        var product_price = submit_btn.data('price');
-        console.log(name);
-        console.log(product_id);
-        console.log(product_price);
-        // language=HTML
+
+
+    function basketUpdating(product_id, qty, is_delete){
+        var data = {};
+        data.product_id = product_id;
+        data.qty = qty;
+
+         var csrf_token = $('#form_buying_product [name="csrfmiddlewaretoken"]').val();
+         data["csrfmiddlewaretoken"] = csrf_token;
+
+        if (is_delete){
+            data["is_delete"] = true;
+        }
+
+         var url = form.attr("action");
+
+        console.log(data)
          $.ajax({
              url: url,
              type: 'POST',
@@ -24,8 +29,39 @@ $(document).ready(function() {
                  console.log("OK");
                  console.log(data.products_total_nmb);
                  if (data.products_total_nmb || data.products_total_nmb == 0){
-        $('.basket-item').append('<li>'+name+', qty: ' + qty +', total price: '+product_price*qty+'UAH'+'<a href="" id="id-delete_item" class="delete_item" >x</a>'+'</li>');
+                    $('#basket_total_nmb').text("("+data.products_total_nmb+")");
+                     console.log(data.products);
+                     $('.basket-items ul').html("");
+                     $.each(data.products, function(k, v){
+                        $('.basket-items ul').append('<li>'+ v.name+', ' + v.qty + 'шт. ' + 'по ' + v.price + 'грн  ' +
+                            '<a class="delete-item" href="" data-product_id="'+v.id+'">x</a>'+
+                            '</li>');
+                         $('.basket-item').append('<li>'+v.name+', qty: ' + v.qty +', total price: '+v.price *v.qty+'UAH'+'<a href="" id="id-delete_item" class="delete_item" >x</a>'+'</li>');
     });
+                     });
+                 }
+
+             },
+             error: function(){
+                 console.log("error")
+             }
+         })
+
+    }
+
+
+
+    form.on('submit',function (e) {
+        e.preventDefault();
+        var qty = $('#number').val();
+
+        var submit_btn = $('#submit-btn');
+        var name = submit_btn.data('name');
+        var product_id = submit_btn.data('product_id');
+        var product_price = submit_btn.data('price');
+
+
+
     $(document).on('click','.delete_item', function (e) {
         e.preventDefault();
         $(this).closest('li').remove();
