bae8c988701ec066ee6fa5d17e716f75545f20f3
ViacheslavKhvorostynyi <slavafh@gmail.com>
2017-08-31 19:38:12 +0300

    edit bash script and delete scriptsvform root

--- scripts.js
+++
@@ -1,6 +0,0 @@
-tree b1f4007a422e61f4ab470ec4f94afdfa00876fbe
-parent b0bb32423bccd9887b68fd34146cdc7075fd4f8a
-author ViacheslavKhvorostynyi <slavafh@gmail.com> 1503947388 +0300
-committer ViacheslavKhvorostynyi <slavafh@gmail.com> 1503947388 +0300
-
-edit navbar/card/close
