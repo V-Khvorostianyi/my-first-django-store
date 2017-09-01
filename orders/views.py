ce09556cb6209c0bfb804f0a115e5cd6edbafebb
ViacheslavKhvorostynyi <slavafh@gmail.com>
2017-09-01 22:20:13 +0300

    edit views

--- orders/views.py
+++ orders/views.py
@@ -6,6 +6,8 @@ def basket_adding(request):
     return_dict = dict()
     session_key = request.session.session_key
     print (request.POST)
-
-    ProductInCard.objects.get(session_key=session_key)
+    data = request.POST
+    product_id = data.get('product_id')
+    qty = data.get('qty')
+    new_product = ProductInCard.objects.get(session_key=session_key,product_id=product_id,qty=qty )
     return JsonResponse(return_dict)
