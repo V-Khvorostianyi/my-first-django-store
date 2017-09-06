e8a523356dfd5313de4d941aa00a003d6c61d9a4
ViacheslavKhvorostynyi <slavafh@gmail.com>
2017-09-06 09:34:17 +0300

    change orders views

--- orders/views.py
+++ orders/views.py
@@ -9,7 +9,7 @@ def basket_adding(request):
     data = request.POST
     product_id = data.get('product_id')
     qty = data.get('qty')
-    new_product = ProductInCard.objects.create(session_key=session_key,product_id=product_id,qty=qty )
+    new_product = ProductInCard.objects.update_or_create(session_key=session_key,product_id=product_id,qty=qty )
     products_in_card = ProductInCard.objects.filter(session_key=session_key, is_active=True)
     products_total_qty = products_in_card.count()
     return_dict["products_total_qty"] = products_total_qty
