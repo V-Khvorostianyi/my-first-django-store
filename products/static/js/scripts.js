e05999fdac6833a8410d099a560a76d847410339
KhvorostyanyiVaicheslav <slavafh@gmail.com>
2017-09-10 09:29:28 +0300

    add func basketUptate

--- products/static/js/scripts.js
+++ products/static/js/scripts.js
@@ -3,18 +3,8 @@ $(document).ready(function() {
     var form;
     form = $('#form_buying_product');
     console.log(form);
-    form.on('submit',function (e) {
-        e.preventDefault();
-        var qty = $('#number').val();
-        console.log(qty);
-        var submit_btn = $('#submit-btn');
-        var name = submit_btn.data('name');
-        var product_id = submit_btn.data('product_id');
-        var product_price = submit_btn.data('price');
-        console.log(name);
-        // console.log(product_id);
-        // console.log(product_price);
 
+    function basketUpdate() {
         var data ={};
         data.product_id = product_id;
         data.qty = qty;
@@ -47,6 +37,23 @@ $(document).ready(function() {
                 console.log('error');
             }
         });
+
+
+
+    }
+
+    form.on('submit',function (e) {
+        e.preventDefault();
+        var qty = $('#number').val();
+        console.log(qty);
+        var submit_btn = $('#submit-btn');
+        var name = submit_btn.data('name');
+        var product_id = submit_btn.data('product_id');
+        var product_price = submit_btn.data('price');
+        // console.log(name);
+        // console.log(product_id);
+        // console.log(product_price);
+        basketUpdate()
 
     });
     $(document).on('click','.delete_item', function (e) {
